fcaf23ebe2ce7eeb5e4c823d66548be7f12d88a6
Ryan Sizemore <58410246+mastaryan@users.noreply.github.com>
2020-05-07 16:25:38 -0400

    Setup basic Announcement Query Structure

---
+++ src/pages/Chat/queries/Announcements.js
@@ -0,0 +1,12 @@
+import gql from "graphql-tag";
+
+// Retrieve announcements
+
+// Send an announcement
+
+// Delete an announcement
+
+// Retrieve a list of recipients
+
+// Announcement Subscriptions
+
